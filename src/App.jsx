a22605163be3b5424d4eb97d47b3e9ad9ddd7fa7
tarti <tartilaci@gmail.com>
2024-05-25 19:20:13 +0200

    "Updated subproject commit reference to indicate dirty state"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import Test from './Test';
 import './app.scss'
 import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return <div>
-    <section><Navbar/></section>
+    {/* <section><Navbar/></section>
     <section>Parallax</section>
     <section>Sercives</section>
     <section>Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section>
+    <section>Contact</section> */}
+    <div><Test/></div>
   </div>;
 };
 
