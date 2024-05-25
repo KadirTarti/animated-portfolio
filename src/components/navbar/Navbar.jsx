12904e84a7d793e25b46fbcb24535baf130a96cf
tarti <tartilaci@gmail.com>
2024-05-25 18:42:01 +0200

    add navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+    {/* Sidebar */}
+    <div className="wrapper">
+        <span>AkadirTartilaci</span>
+        <div className="social">
+            <a href="">
+                
+            </a>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
