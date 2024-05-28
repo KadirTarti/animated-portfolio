6fe3f4923a73f968c4892d1cba2738873386d1bd
tarti <tartilaci@gmail.com>
2024-05-28 14:18:24 +0200

    add properties to links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,28 +4,28 @@ import Links from './links/Links'
 import './sidebar.scss'
 import ToggleButton from './toggleButton/ToggleButton'
 
+const variants = {
+    open: {
+        clipPath: 'circle(1000px at 50px 50px)',
+        transition: {
+            type: 'spring',
+            stiffness: 20,
+        }
+    },
+    closed: {
+        clipPath: 'circle(30px at 50px 50px)',
+        transition: {
+            delay: 0.5,
+            type: 'spring',
+            stiffness:400,
+            damping: 40,
+        }
+    }
+}
 const Sidebar = () => {
 
     const [open, setOpen] = useState(false)
 
-    const variants = {
-        open: {
-            clipPath: 'circle(1000px at 50px 50px)',
-            transition: {
-                type: 'spring',
-                stiffness: 20,
-            }
-        },
-        closed: {
-            clipPath: 'circle(30px at 50px 50px)',
-            transition: {
-                delay: 0.5,
-                type: 'spring',
-                stiffness:400,
-                damping: 40,
-            }
-        }
-    }
 
 
 
