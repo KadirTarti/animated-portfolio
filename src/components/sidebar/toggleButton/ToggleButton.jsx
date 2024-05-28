7efb50775205c028642d71ec7ab4343823981c82
tarti <tartilaci@gmail.com>
2024-05-28 13:56:33 +0200

    fix toggle button animate

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -3,22 +3,31 @@ import { motion } from "framer-motion";
 const ToggleButton = ({ setOpen }) => {
   return (
     <button onClick={() => setOpen((e) => !e)}>
-      <svg width="23" height="23" viewBox="0 0 23 23">
+      <svg style={{marginTop:'7px'}} width="23" height="23" viewBox="0 0 23 23">
         <motion.path
           strokeWidth="3"
           stroke="black"
           strokeLinecap="round"
-          variant={{ 
+          variants={{ 
             closed: { d: "M 2 2.5 L 20 2.5" }, 
             open: {d: "M 3 16.5 L 17 2.5"},
            }}
         />
-        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round" />
+        <motion.path strokeWidth="3" stroke="black" strokeLinecap="round"
+        d= "M 2 9.5 L 20 9.5"
+        variants={{ 
+          closed: { opacity:1 },
+          open: {opacity: 0}
+                   }}
+            />
         <motion.path
           strokeWidth="3"
           stroke="black"
           strokeLinecap="round"
-          variant={{ closed: { d: "M 2 2.5 L 20 2.5" },  open: {d: "M 3 16.5 L 17 2.5"}, }}
+          variants={{ 
+            closed: { d: "M 2 16.3 L 20 16.3" }, 
+            open: {d: "M 3 2.5 L 17 16.3"},
+           }}
         />
       </svg>
     </button>
